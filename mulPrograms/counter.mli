5101a8dff9fc220a46769b61e87e0654a68928e5
Yansy <yansiyu.old.dog@hotmail.com>
2021-07-12 15:34:20 +0800

    sig

---
+++ mulPrograms/counter.mli
@@ -0,0 +1,19 @@
+(*  Here’s an abstract interface for Counter: *)
+open Base
+
+
+(* A collection of string frequency counts. *)
+type t
+
+(* The empty set of frequency counts *)
+val empty : t
+
+(* Bump the frequency count for the given string.*)
+val touch : t -> string -> t
+
+(* Converts the set of frequeny counts to an association list. 
+   A String shows up at most once, and the counts are >=1. *)
+val to_list : t -> (string * int) list
+
+
+
